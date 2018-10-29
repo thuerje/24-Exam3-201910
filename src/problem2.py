2de24a22462096db91a7d6feeabb0413368fce57
thuerje <thuerje@rose-hulman.edu>
2018-10-29 00:02:02 -0400

    problem2

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Jess Thuer.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,10 +90,20 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    num = 0
+    kk = 0
+    import math
+
+    for k in range(len(sequence)):
+        index = math.fabs((sequence[k]))
+        if index > num:
+            num = index
+            kk = k
+    return kk
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
